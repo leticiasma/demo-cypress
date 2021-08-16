745978cccf5ef6662def2654a7a7ec236129dbbd
leticiasma <leti.ufmg@gmail.com>
2021-08-16 16:37:08 -0300

    Tarefas 3 e 4

--- cypress/integration/meu_teste_end_to_end.js
+++ cypress/integration/meu_teste_end_to_end.js
@@ -16,5 +16,15 @@ describe('Teste End-to-End', () => {
 
         // Fecha o pop-up com o preço do frete
         cy.get('.swal-button').click()
+
+	//  Compra o livro desejado
+	cy.get('[data-id=3]').within(() => {
+           cy.contains('Comprar').click().then
+           cy.wait(2000)
+        })
+	cy.get('.swal-text').contains('Sua compra foi realizada com sucesso')
+
+	// Fecha o pop-up com a confirmação da compra
+        cy.get('.swal-button').click()
     })
   })
